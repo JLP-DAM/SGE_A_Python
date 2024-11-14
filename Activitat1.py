2578bbea31da4bd8bcba0538fa4ebc67cb22c955
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2024-11-14 11:43:15 +0100

    Delete Activitat1.py de main

--- Activitat1.py
+++
@@ -1,9 +0,0 @@
-Nombre1 = input()
-Nombre2 = input()
-
-if Nombre1 > Nombre2:
-    print(Nombre1 + " es mes petit que " + Nombre2)
-elif Nombre2 > Nombre1:
-    print(Nombre2 + " es mes gran que " + Nombre1)
-else:
-    print(Nombre2 + " es igual a " + Nombre1)
